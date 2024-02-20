b8593fb72382e494d540bd15bc0e427de9062be5
Afqufai <afrizalquratulfaizih@gmail.com>
2024-02-20 09:22:05 +0700

    P2_Percobaan 3_Pertanyaan 3_03

--- src/P2/Buku03.java
+++ src/P2/Buku03.java
@@ -4,10 +4,6 @@ public class Buku03 {
     String judul, pengarang;
     int halaman, stok, harga;
 
-    public Buku03(){
-        
-    }
-
     public Buku03(String jud, String pg, int hal, int stok, int har){
         judul = jud;
         pengarang = pg;
