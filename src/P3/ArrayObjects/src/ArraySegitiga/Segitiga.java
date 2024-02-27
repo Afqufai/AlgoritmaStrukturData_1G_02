694cf3cd3515bb46b2e19c95493636d1796e3d33
Afqufai <afrizalquratulfaizih@gmail.com>
2024-02-27 18:06:40 +0700

    P3_Percobaan 3_Pertanyaan 3_03

--- src/P3/ArrayObjects/src/ArraySegitiga/Segitiga.java
+++ src/P3/ArrayObjects/src/ArraySegitiga/Segitiga.java
@@ -1,4 +1,5 @@
 package P3.ArrayObjects.src.ArraySegitiga;
+import java.lang.Math;
 
 public class Segitiga {
     public int alas;
@@ -11,5 +12,14 @@ public class Segitiga {
     public Segitiga(int a, int t){
         alas = a;
         tinggi = t;
+    }
+
+    public int hitungLuas(){
+        return alas * tinggi / 2;
+    }
+
+    public int hitungKeliling(){
+        int sisiMiring = (int) Math.sqrt((alas * alas) + (tinggi * tinggi));
+        return sisiMiring + alas + tinggi;
     }
 }
