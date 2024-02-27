5947f9287ee97400527a2c68e267291461b46564
Afqufai <afrizalquratulfaizih@gmail.com>
2024-02-27 18:05:35 +0700

    P3_Percobaan 3_Pertanyaan 2_03

---
+++ src/P3/ArrayObjects/src/ArraySegitiga/Segitiga.java
@@ -0,0 +1,15 @@
+package P3.ArrayObjects.src.ArraySegitiga;
+
+public class Segitiga {
+    public int alas;
+    public int tinggi;
+
+    //Default
+    public Segitiga(){}
+
+    //Berparameter
+    public Segitiga(int a, int t){
+        alas = a;
+        tinggi = t;
+    }
+}
