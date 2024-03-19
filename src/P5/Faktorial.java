57cdc0eadf8a3f63298ae2d6dd0670c62f81102c
Afqufai <afrizalquratulfaizih@gmail.com>
2024-03-19 19:41:19 +0700

    P5_Percobaan 1_Pertanyaan 2_03

--- src/P5/Faktorial.java
+++ src/P5/Faktorial.java
@@ -5,8 +5,10 @@ public class Faktorial {
 
     int faktorialBF(int n){
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto * i;
+        int i = 1;
+        while (n >= i) {
+            fakto *= i;
+            i++;
         }
         return fakto;
     }
