feae0071056bccd39a28f5a2b0bb3a71af4e079e
Afqufai <afrizalquratulfaizih@gmail.com>
2024-03-20 15:37:40 +0700

    P5_Percobaan 2_Pertanyaan 4_03

--- src/P5/MainPangkat.java
+++ src/P5/MainPangkat.java
@@ -19,16 +19,32 @@ public class MainPangkat{
             png[i] = new Pangkat(nilai, pangkat);
         }
 
-        System.out.println("HASIL PANGKAT - BRUTE FORCE: ");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
-        }
+        System.out.println("Dengan metode apa anda ingin melakukan perpengkatan?");
+        System.out.println("================================");
+        System.out.println("[1] - BRUTE FORCE");
+        System.out.println("[2] - DIVIDE AND CONQUER");
+        System.out.println("================================");
+        System.out.print(">> ");
+        int menuSel = input03.nextInt();
 
-        System.out.println("HASIL PANGKAT - DIVIDE AND CONQUER: ");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+        switch (menuSel) {
+            case 1:
+            System.out.println("HASIL PANGKAT - BRUTE FORCE: ");
+            for (int i = 0; i < elemen; i++) {
+                System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+            }
+                break;
+            case 2:
+            System.out.println("HASIL PANGKAT - DIVIDE AND CONQUER: ");
+            for (int i = 0; i < elemen; i++) {
+                System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+            }
+                break;
+            default:
+            System.out.println("INVALID INPUT");
+                break;
         }
-
+        
         input03.close();
     }
 }
