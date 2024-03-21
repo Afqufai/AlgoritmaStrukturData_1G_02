e0f1808037bfd1e80fddf3c42f83efe967f02616
Afqufai <afrizalquratulfaizih@gmail.com>
2024-03-21 11:59:17 +0700

    P5_Percobaan 3_03

---
+++ src/P5/MainSum.java
@@ -0,0 +1,30 @@
+package P5;
+import java.util.Scanner;
+
+public class MainSum {
+    public static void main(String[] args) {
+        Scanner input03 = new Scanner(System.in);
+
+        System.out.println("==============================");
+        System.out.println("Program Menghitung Keuntungan Total");
+        System.out.println("(Satuan juta, Misal 5.9)");
+        System.out.println("==============================");
+        System.out.print("Masukkan Jumlah Bulan: ");
+        int elm = input03.nextInt();
+        Sum sm = new Sum(elm);
+
+        System.out.println("==============================");
+        for (int i = 0; i < sm.elemen; i++) {
+            System.out.print("Masukkan keuntungan bulan ke-" + (i+1) + ": ");
+            sm.keuntungan[i] = input03.nextDouble();
+        }
+
+        System.out.println("Algoritma Brute Force:");
+        System.out.println("Total keuntungan perusahaan selama " + sm.elemen + " bulan adalah " + sm.totalBF(sm.keuntungan));
+        System.out.println("=======================================");
+        System.out.println("Algoritma Divide and Conquer:");
+        System.out.println("Total keuntungan perusahaan selama " + sm.elemen + " bulan adalah " + sm.totalDC(sm.keuntungan, 0, sm.elemen - 1));
+
+        input03.close();
+    }
+}
