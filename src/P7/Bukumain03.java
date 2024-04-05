384b18b0e046a9a630282641eb8b4a8a56afbe3e
Afqufai <afrizalquratulfaizih@gmail.com>
2024-04-05 16:43:20 +0700

    P7_Percobaan 1_03

---
+++ src/P7/Bukumain03.java
@@ -0,0 +1,46 @@
+package P7;
+import java.util.Scanner;
+
+public class Bukumain03 {
+    public static void main(String[] args) {
+        Scanner sc03S = new Scanner(System.in);
+        Scanner sc03I = new Scanner(System.in);
+    
+        PencarianBuku03 data = new PencarianBuku03();
+        int jumBuku = 5;
+
+        System.out.println("=======================================");
+        System.out.println("Masukkan data buku secara urut dari kode buku terkecil:");
+        for (int i = 0; i < jumBuku; i++) {
+            System.out.println("=======================================");
+            System.out.print("Masukkan kode buku: ");
+            int kode = sc03I.nextInt();
+            System.out.print("Masukkan judul buku: ");
+            String jud = sc03S.nextLine();
+            System.out.print("Masukkan tahun terbit: ");
+            int tahun = sc03I.nextInt();
+            System.out.print("Masukkan nama pengarang: ");
+            String pengarang = sc03S.nextLine();
+            System.out.print("Stok: ");
+            int stok = sc03I.nextInt();
+
+            Buku03 m = new Buku03(kode, jud, tahun, pengarang, stok);
+            data.tambah(m);
+        }
+        System.out.println("=======================================");
+        System.out.println("Data keseluruhan buku: ");
+        data.tampil();
+
+        System.out.println("=======================================");
+        System.out.println("Pencarian data: ");
+        System.out.println("Masukkan kode buku yang dicari: ");
+        System.out.print("Kode buku: ");
+        int cari = sc03I.nextInt();
+        System.out.println("Menggunakan sequential searching...");
+        int posisi = data.FindSeqSearch(cari);
+        data.Tampilposisi(cari, posisi);
+
+        sc03S.close();
+        sc03I.close();
+    }
+}
---
+++ src/P7/PencarianBuku03.java
@@ -0,0 +1,48 @@
+package P7;
+
+public class PencarianBuku03 {
+    Buku03[] listBk = new Buku03[5];
+    int idx = 0;
+
+    void tambah(Buku03 m){
+        if (idx < listBk.length) {
+            listBk[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!!!");
+        }
+    }
+    
+    void tampil(){
+        for (Buku03 m : listBk) {
+            m.tampilDataBuku();
+        }
+    }
+
+    int FindSeqSearch(int cari){
+        int posisi = 0;
+        for (int i = 0; i < listBk.length; i++) {
+            if (listBk[i].kodeBuku == cari) {
+                posisi = i;
+                break;
+            } else {
+                posisi = -1;
+            }
+        }
+        return posisi;
+    }
+
+    void Tampilposisi(int x, int pos){
+        if (pos != -1) {
+            System.out.println("===================================");
+            System.out.println("Data" + x +" ditemukan pada index " + pos + "!");
+            System.out.println("Kode Buku: " + x);
+            System.out.println("Judul Buku: " + listBk[pos].judulBuku);
+            System.out.println("Tahun Terbit: " + listBk[pos].tahunTerbit);
+            System.out.println("Pengarang: " + listBk[pos].pengarang);
+            System.out.println("Stock: " + listBk[pos].stock);
+        } else {
+            System.out.println("Data " + x + " tidak ditemukan!");
+        }
+    }
+}
