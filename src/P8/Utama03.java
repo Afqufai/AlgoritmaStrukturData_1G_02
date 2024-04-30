3afa2caea46b21fdc37abff32225a86030d5e766
Afqufai <afrizalquratulfaizih@gmail.com>
2024-04-30 13:53:47 +0700

    P8_Percobaan 1_Pertanyaan 4_03

--- src/P8/Utama03.java
+++ src/P8/Utama03.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 
 public class Utama03 {
     public static void main(String[] args) {
-        Gudang03 gudang = new Gudang03(7);
         Scanner sc03 = new Scanner(System.in);
+        boolean quit = false;
 
-        while (true) {
+        System.out.print("Masukkan kapasitas barang: ");
+        int cap = sc03.nextInt();
+        Gudang03 gudang = new Gudang03(cap);
+
+        while (!quit) {
             System.out.println("\nMenu");
             System.out.println("1. Tambah barang");
             System.out.println("2. Ambil barang");
             System.out.println("3. Tampilkan tumpukan barang");
-            System.out.println("4. Keluar");
+            System.out.println("4. Tampil barang teratas");
+            System.out.println("5. Keluar");
             System.out.print("Pilih Operasi: ");
             int menu = sc03.nextInt();
 
@@ -34,6 +39,9 @@ public class Utama03 {
                     gudang.tampilkanBarang();
                     break;
                 case 4:
+                    gudang.lihatBarangTeratas();
+                case 5:
+                    quit = true;
                     break;
                 default:
                     System.out.println("Pilihan tidak valid, silahkan coba lagi");
