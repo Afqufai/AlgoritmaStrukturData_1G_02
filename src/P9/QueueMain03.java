e65aed875f4fc3200de17ad0bc29f2c85657bc87
Afqufai <afrizalquratulfaizih@gmail.com>
2024-04-30 18:49:19 +0700

    P9_Percobaan 1_03

---
+++ src/P9/QueueMain03.java
@@ -0,0 +1,55 @@
+package P9;
+import java.util.Scanner;
+
+public class QueueMain03 {
+    static void menu(){
+        System.out.println("Masukkan operasi yang di inginkan");
+        System.out.println("1. Enqueue");
+        System.out.println("2. Dequeue");
+        System.out.println("3. Print");
+        System.out.println("4. Peek");
+        System.out.println("5. Clear");
+        System.out.println("Angka lain untuk keluar");
+        System.out.println("====================================");
+    }
+    public static void main(String[] args) {
+        Scanner sc03 = new Scanner(System.in);
+        System.out.print("Masukkan kapasitas queue: ");
+        int n = sc03.nextInt();
+        Queue03 Q = new Queue03(n);
+
+        int pilih = 1;
+
+        do {
+            menu();
+            pilih = sc03.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Masukkan data baru: ");
+                    int dataMasuk = sc03.nextInt();
+                    Q.Enqueue(dataMasuk);
+                    break;
+                case 2:
+                    int dataKeluar = Q.Dequeue();
+                    if (dataKeluar != 0) {
+                        System.out.println("Data yang dikeluarkan: " + dataKeluar);
+                        break;
+                    }
+                    break;
+                case 3:
+                    Q.print();
+                    break;
+                case 4:
+                    Q.peek();
+                    break;
+                case 5:
+                    Q.clear();
+                    break;
+                default:
+                    break;
+            }
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih ==4 || pilih == 5);
+
+        sc03.close();
+    }
+}
