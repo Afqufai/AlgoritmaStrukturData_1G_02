b22a3e5e2d67ceb268cb05d5f4f9e9d32d0ce815
Afqufai <afrizalquratulfaizih@gmail.com>
2024-05-14 17:03:52 +0700

    P11_Percobaan 2_03

--- src/P11/doublelinkedlists/DoubleLinkedLists.java
+++ src/P11/doublelinkedlists/DoubleLinkedLists.java
@@ -86,5 +86,58 @@ public class DoubleLinkedLists {
         }
     }
 
+    void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong! Data tidak dapat dihapus!");
+        } else if (size == 1) {
+            removeLast();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong! Data tidak dapat dihapus!");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    void remove(int index) throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong! Data tidak dapat dihapus!");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else if (current.prev == null) {
+                current = current.next;
+                current.prev = null;
+                head = current;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
+        }
+    }
 
 }
