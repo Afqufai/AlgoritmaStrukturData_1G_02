5593dba63e8e0647cd5760d6bf5b8af0b7a14ef2
Afqufai <afrizalquratulfaizih@gmail.com>
2024-05-29 18:42:51 +0700

    P12_Percobaan 1_03

---
+++ src/P12/Node03.java
@@ -0,0 +1,13 @@
+package P12;
+
+public class Node03{
+    int data;
+    Node03 left, right;
+
+    public Node03(){}
+    public Node03(int data){
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
